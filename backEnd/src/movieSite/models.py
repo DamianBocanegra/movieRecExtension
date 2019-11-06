1c54450f2d9ed59ebf3cded96f42b4ca0a7b3a3a
Damian Bocanegra <d-b172@users.noreply.github.com>
2019-11-06 12:05:07 -0600

    More setup and added movie model

--- backEnd/src/movieSite/models.py
+++ backEnd/src/movieSite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    filmID = models.IntegerField()
+    title =  models.CharField(max_length=120)
+    genre = models.CharField(max_length = 12)
+    releaseYear = models.CharField(max_length = 4)
+    length = models.IntegerField()
+    MPAA_rating = models.CharField(max_length = 6)
+    qualityRating = models.IntegerField();
