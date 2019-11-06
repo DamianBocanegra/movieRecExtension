abbaf0473a79d8661b0169f04f549cdb4159c36e
Damian Bocanegra <d-b172@users.noreply.github.com>
2019-11-06 13:11:31 -0600

    Set up layout and list view

---
+++ frontEnd/my-app/src/container/FilmsListView.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Films from '../components/films'
+
+class FilmListView extends React.Component{
+
+    render(){
+        return(
+            <Films />
+        );
+    }
+}
+export default FilmListView
